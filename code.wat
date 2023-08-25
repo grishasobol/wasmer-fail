7bf9784da09b827be2bf73929db4fb609837ccaf
Gregory Sobol <grishasobol@mail.ru>
2023-08-25 15:08:56 +0400

    final for stack overflow bug

--- code.wat
+++ code.wat
@@ -1,84 +1,12 @@
 (module
-  (type (;0;) (func))
-  (type (;1;) (func (param i32)))
-  (type (;2;) (func (param i32 i32)))
-  (type (;3;) (func (param i32 i32 i32 i32 i32)))
-  (type (;4;) (func (param i32 i32 i32 i32)))
-  (type (;5;) (func (param i32 i32 i32)))
-  (type (;6;) (func (result i64)))
-  (import "env" "memory" (memory (;0;) 261))
-  (import "env" "gr_reply_push_input" (func (;0;) (type 5)))
-  (import "env" "gr_debug" (func (;1;) (type 2)))
-  (import "env" "gr_out_of_gas" (func (;2;) (type 0)))
-  (func (;3;) (type 0)
-    (local i64)
-    i32.const 15820
-    call 7
-    global.get 0
-    i32.const 28286
-    i32.const 16845
-    i32.const 13746590
-    call 0
-    i32.const 192
-    call 7
-    drop
+  (import "env" "import" (func $imp))
+  (func $init
+    (i64.add (global.get $gas) (i64.const 1))
+    global.set $gas
+    call $imp
+    call $init
   )
-  (func (;4;) (type 6)
-    loop  ;; label = @1
-      i32.const 1487
-      call 7
-      i32.const 4424
-      call 7
-      call 5
-      i32.const 3138
-      call 7
-      br 0 (;@1;)
-    end
-    unreachable
-  )
-  (func (;5;) (type 0)
-    block  ;; label = @1
-      i32.const 28571
-      call 7
-      call 3
-      call 5
-      br 0 (;@1;)
-    end
-  )
-  (func (;6;) (type 0)
-    i32.const 9232
-    call 7
-    i32.const 16777216
-    i32.const 41
-    call 1
-    i32.const 12541
-    call 7
-    call 4
-    drop
-    unreachable
-  )
-  (func (;7;) (type 1)
-    (local i64)
-    local.get 0
-    i64.extend_i32_u
-    i64.const 26076
-    i64.add
-    local.tee 1
-    global.get 2
-    i64.gt_u
-    if  ;; label = @1
-      call 2
-    end
-    global.get 2
-    local.get 1
-    i64.sub
-    global.set 2
-  )
-  (global (;0;) (mut i64) (i64.const -6165552189891260671))
-  (global (;1;) i32 (i32.const 16777216))
-  (global (;2;) (mut i64) (i64.const 0))
-  (export "__gear_stack_end" (global 1))
-  (export "init" (func 6))
-  (export "gear_gas" (global 2))
-  (data (;0;) (i32.const 16777216) "Gear program seed = '3161054873836996002'")
+  (global $gas (mut i64) (i64.const 0))
+  (export "init" (func $init))
+  (export "gas" (global $gas))
 )
--- src/lib.rs
+++ src/lib.rs
@@ -1,201 +1,191 @@
 // These tests must stuck in a loop on M1 apple processor and singlepass compiler.
 
-#[test]
-fn test_popcnt_i32() {
-    use wasmer::{imports, Instance, Module, Store, Value};
+// #[test]
+// fn test_popcnt_i32() {
+//     use wasmer::{imports, Instance, Module, Store, Value};
 
-    let get_next_number_i32 = |mut num: i32| {
-        num ^= num << 13;
-        num ^= num >> 17;
-        num ^= num << 5;
-        num
-    };
+//     let get_next_number_i32 = |mut num: i32| {
+//         num ^= num << 13;
+//         num ^= num >> 17;
+//         num ^= num << 5;
+//         num
+//     };
 
-    let module_wat = r#"
-    (module
-        (func $popcnt (export "popcnt") (param i32) (result i32)
-            local.get 0
-            i32.popcnt
-        )
-    )"#;
+//     let module_wat = r#"
+//     (module
+//         (func $popcnt (export "popcnt") (param i32) (result i32)
+//             local.get 0
+//             i32.popcnt
+//         )
+//     )"#;
 
-    let store = Store::default();
-    let import_object = imports! {};
-    let module = Module::new(&store, &module_wat).unwrap();
-    let instance = Instance::new(&module, &import_object).unwrap();
+//     let store = Store::default();
+//     let import_object = imports! {};
+//     let module = Module::new(&store, &module_wat).unwrap();
+//     let instance = Instance::new(&module, &import_object).unwrap();
 
-    let popcnt = instance.exports.get_function("popcnt").unwrap();
+//     let popcnt = instance.exports.get_function("popcnt").unwrap();
 
-    let mut num = 1;
-    for _ in 1..10000 {
-        let result = popcnt.call(&[Value::I32(num)]).unwrap();
-        assert_eq!(&Value::I32(num.count_ones() as i32), result.get(0).unwrap());
-        num = get_next_number_i32(num);
-    }
-}
+//     let mut num = 1;
+//     for _ in 1..10000 {
+//         let result = popcnt.call(&[Value::I32(num)]).unwrap();
+//         assert_eq!(&Value::I32(num.count_ones() as i32), result.get(0).unwrap());
+//         num = get_next_number_i32(num);
+//     }
+// }
 
-#[test]
-fn test_popcnt_i64() {
-    use wasmer::{imports, Instance, Module, Store, Value};
+// #[test]
+// fn test_popcnt_i64() {
+//     use wasmer::{imports, Instance, Module, Store, Value};
 
-    let get_next_number_i64 = |mut num: i64| {
-        num ^= num << 34;
-        num ^= num >> 40;
-        num ^= num << 7;
-        num
-    };
+//     let get_next_number_i64 = |mut num: i64| {
+//         num ^= num << 34;
+//         num ^= num >> 40;
+//         num ^= num << 7;
+//         num
+//     };
 
-    let module_wat = r#"
-    (module
-        (func $popcnt (export "popcnt") (param i64) (result i64)
-            local.get 0
-            i64.popcnt
-        )
-    )"#;
+//     let module_wat = r#"
+//     (module
+//         (func $popcnt (export "popcnt") (param i64) (result i64)
+//             local.get 0
+//             i64.popcnt
+//         )
+//     )"#;
 
-    let store = Store::default();
-    let import_object = imports! {};
-    let module = Module::new(&store, &module_wat).unwrap();
-    let instance = Instance::new(&module, &import_object).unwrap();
+//     let store = Store::default();
+//     let import_object = imports! {};
+//     let module = Module::new(&store, &module_wat).unwrap();
+//     let instance = Instance::new(&module, &import_object).unwrap();
 
-    let popcnt = instance.exports.get_function("popcnt").unwrap();
+//     let popcnt = instance.exports.get_function("popcnt").unwrap();
 
-    let mut num = 1;
-    for _ in 1..10000 {
-        let result = popcnt.call(&[Value::I64(num)]).unwrap();
-        assert_eq!(&Value::I64(num.count_ones() as i64), result.get(0).unwrap());
-        num = get_next_number_i64(num);
-    }
-}
+//     let mut num = 1;
+//     for _ in 1..10000 {
+//         let result = popcnt.call(&[Value::I64(num)]).unwrap();
+//         assert_eq!(&Value::I64(num.count_ones() as i64), result.get(0).unwrap());
+//         num = get_next_number_i64(num);
+//     }
+// }
 
-#[test]
-fn test_ctz() {
-    use wasmer::{imports, Instance, Module, Store, Value};
-    let module_wat = r#"
-    (module
-        (func (export "lol") (param i32) (result i32)
-            i32.const 0xDEADBEEF
-            i32.ctz
-            (block
-                (block
-                    (block
-                        br 1
-                        i32.const 0xdeadbaaf
-                        i32.ctz
-                        drop
-                    )
-                    i32.const 0xdeadbaac
-                    i32.ctz
-                    drop
-                )
-                i32.const 0xdeadfaaf
-                i32.ctz
-                drop
-            )
-            drop
-            i32.const 0xDEADBEEF
-            i32.ctz
-        )
-    )"#;
+// #[test]
+// fn test_ctz() {
+//     use wasmer::{imports, Instance, Module, Store, Value};
+//     let module_wat = r#"
+//     (module
+//         (func (export "lol") (param i32) (result i32)
+//             i32.const 0xDEADBEEF
+//             i32.ctz
+//             (block
+//                 (block
+//                     (block
+//                         br 1
+//                         i32.const 0xdeadbaaf
+//                         i32.ctz
+//                         drop
+//                     )
+//                     i32.const 0xdeadbaac
+//                     i32.ctz
+//                     drop
+//                 )
+//                 i32.const 0xdeadfaaf
+//                 i32.ctz
+//                 drop
+//             )
+//             drop
+//             i32.const 0xDEADBEEF
+//             i32.ctz
+//         )
+//     )"#;
 
-    let store = Store::default();
-    let import_object = imports! {};
-    let module = Module::new(&store, &module_wat).unwrap();
-    let instance = Instance::new(&module, &import_object).unwrap();
+//     let store = Store::default();
+//     let import_object = imports! {};
+//     let module = Module::new(&store, &module_wat).unwrap();
+//     let instance = Instance::new(&module, &import_object).unwrap();
 
-    let lol = instance.exports.get_function("lol").unwrap();
-    println!("{:?}", lol.call(&[Value::I32(10)]));
-}
+//     let lol = instance.exports.get_function("lol").unwrap();
+//     println!("{:?}", lol.call(&[Value::I32(10)]));
+// }
 
-#[test]
-fn check_wasm_execution_stack() {
-    use wasmer::{imports, Instance, Module, Store, Value};
-    let module_wat = r#"
-    (module
-        (func (export "lol") (param i64) (result i64)
-            global.get 0
-            local.get 0
-            i64.add
-            global.set 0
-            (call 0 (local.get 0))
-            drop
-            global.get 0
-        )
-        (global (mut i64) (i64.const 1))
-        (export "global" (global 0))
-    )
-    "#;
+// #[test]
+// fn check_wasm_execution_stack() {
+//     use wasmer::{imports, Instance, Module, Store, Value};
+//     let module_wat = r#"
+//     (module
+//         (func (export "lol") (param i64) (result i64)
+//             global.get 0
+//             local.get 0
+//             i64.add
+//             global.set 0
+//             (call 0 (local.get 0))
+//             drop
+//             global.get 0
+//         )
+//         (global (mut i64) (i64.const 1))
+//         (export "global" (global 0))
+//     )
+//     "#;
 
-    let store = Store::default();
-    let import_object = imports! {};
-    let module = Module::new(&store, &module_wat).unwrap();
-    let instance = Instance::new(&module, &import_object).unwrap();
+//     let store = Store::default();
+//     let import_object = imports! {};
+//     let module = Module::new(&store, &module_wat).unwrap();
+//     let instance = Instance::new(&module, &import_object).unwrap();
 
-    let lol = instance.exports.get_function("lol").unwrap();
-    println!("{:?}", lol.call(&[Value::I64(1)]));
-    println!("{:?}", instance.exports.get_global("global"));
-}
+//     let lol = instance.exports.get_function("lol").unwrap();
+//     println!("{:?}", lol.call(&[Value::I64(1)]));
+//     println!("{:?}", instance.exports.get_global("global"));
+// }
 
-#[test]
-fn check_wasm_execution_stack1() {
-    use wasmer::{imports, Function, Instance, Module, Store, Value};
-    let module_wat = r#"
-    (module
-        (import "env" "add" (func $add (param i64 i64) (result i64)))
-        (func (export "lol") (param i64) (result i64)
-            (call 0 (global.get 0) (local.get 0))
-            (call 0 (global.get 0))
-            (call 0 (global.get 0))
-            (call 0 (global.get 0))
-            global.set 0
-            (call 1 (local.get 0))
-        )
-        (global (mut i64) (i64.const 1))
-        (export "global" (global 0))
-    )
-    "#;
+// #[test]
+// fn check_wasm_execution_stack1() {
+//     use wasmer::{imports, Function, Instance, Module, Store, Value};
+//     let module_wat = r#"
+//     (module
+//         (import "env" "add" (func $add (param i64 i64) (result i64)))
+//         (func (export "lol") (param i64) (result i64)
+//             (call 0 (global.get 0) (local.get 0))
+//             (call 0 (global.get 0))
+//             (call 0 (global.get 0))
+//             (call 0 (global.get 0))
+//             global.set 0
+//             (call 1 (local.get 0))
+//         )
+//         (global (mut i64) (i64.const 1))
+//         (export "global" (global 0))
+//     )
+//     "#;
 
-    let store = Store::default();
-    let add = |x: i64, y: i64| x + y;
-    let import_object = imports! {
-        "env" => {
-            "add" => Function::new_native(&store, add)
-        },
-    };
-    let module = Module::new(&store, &module_wat).unwrap();
-    let instance = Instance::new(&module, &import_object).unwrap();
+//     let store = Store::default();
+//     let add = |x: i64, y: i64| x + y;
+//     let import_object = imports! {
+//         "env" => {
+//             "add" => Function::new_native(&store, add)
+//         },
+//     };
+//     let module = Module::new(&store, &module_wat).unwrap();
+//     let instance = Instance::new(&module, &import_object).unwrap();
 
-    let lol = instance.exports.get_function("lol").unwrap();
-    println!("{:?}", lol.call(&[Value::I64(1)]));
-    println!("{:?}", instance.exports.get_global("global"));
-}
+//     let lol = instance.exports.get_function("lol").unwrap();
+//     println!("{:?}", lol.call(&[Value::I64(1)]));
+//     println!("{:?}", instance.exports.get_global("global"));
+// }
 
 #[test]
 fn check_wasm_execution_stack2() {
-    use wasmer::{imports, Function, Instance, Memory, MemoryType, Module, Store, Val};
+    use wasmer::{imports, Function, Instance, Module, Store};
 
     let wat = std::fs::read("code.wat").unwrap();
 
-    let store = Store::default();
+    let mut store = Store::default();
     let import_object = imports! {
         "env" => {
-            "memory" => Memory::new(&store, MemoryType::new(261, None, false)).unwrap(),
-            "gr_reply_push_input" => Function::new_native(&store, |_x: i32, _y: i32, _z: i32| {}),
-            "gr_debug" => Function::new_native(&store, |_x: i32, _y: i32| {}),
-            "gr_out_of_gas" => Function::new_native(&store, || { println!("Out of gas")}),
+            "import" => Function::new_typed(&mut store, || {}),
         },
     };
     let module = Module::new(&store, &wat).unwrap();
-    let instance = Instance::new(&module, &import_object).unwrap();
-
-    instance
-        .exports
-        .get_global("gear_gas")
-        .unwrap()
-        .set(Val::I64(250_000_000_000))
-        .unwrap();
+    let instance = Instance::new(&mut store, &module, &import_object).unwrap();
 
     let init = instance.exports.get_function("init").unwrap();
-    println!("{:?}", init.call(&[]));
-    println!("{:?}", instance.exports.get_global("gear_gas"));
+    println!("{:?}", init.call(&mut store, &[]));
+    println!("{:?}", instance.exports.get_global("gas").unwrap().get(&mut store));
 }
